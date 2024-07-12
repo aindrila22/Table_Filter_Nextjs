8210f883af6b49b84167a8f33721ac4bf137598c
Aindrila Bhattacharjee <aindrilabhattacharjee72@gmail.com>
2024-07-12 21:58:12 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TabularData from "@/components/TabularData";
 
 export default function Home() {
   return (
-    <main className="bg-black min-h-full">
+    <main className="bg-black h-full min-h-screen">
       <TabularData />
     </main>
   );
